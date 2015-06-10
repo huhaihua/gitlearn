5831dd06d0799c9e3dbe8d36cd9a25eb59f01e38
huhaihua <b55597@MPUMM-SERVER.(none)>
2015-06-10 11:05:58 +0800

    and simple

--- test.c
+++ test.c
@@ -2,4 +2,4 @@ git is a distributed version of control system
 git is free software distributed under GPL
 Git has a mutable index called stage
 git tracks changes of file
-Creating a new branch is quick
+Creating a new branch is quick and simple
