2074eb0c4a06ff30b91d57d693c36edafc433d63
huhaihua <b55597@MPUMM-SERVER.(none)>
2015-06-10 12:28:17 +0800

    fix bug

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
 git is a distributed version of control system
-git is free software distributed under GPL
+git is a free software distributed under GPL
 Git has a mutable index called stage
 git tracks changes of file
 Creating a new branch is quick & simple
